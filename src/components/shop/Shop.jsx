c4da224296cf68593dc3e9ebb6eba90d65d86290
Safoun <safoun.official@gmail.com>
2023-03-27 15:00:01 +0600

    added and tested connection of shop

---
+++ src/components/shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ("./Shop.css")
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                <div>Products coming here !!!!</div>
+            </div>
+            <div className='cart-container'>
+                <div>Order summary !!!</div>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
