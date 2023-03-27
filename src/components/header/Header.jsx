a6cf5d9d2a962b45e03dd838d881c4cb3e4fe038
Safoun <safoun.official@gmail.com>
2023-03-27 14:06:37 +0600

    addeded the hedaer portion of the site

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import("./Header.css");
+import logo from "../../images/Logo.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={logo} alt="logo" />
+      <div>
+        <a href="/order">Order</a>
+        <a href="/review">Order Review</a>
+        <a href="/inventory">Manage Inventory</a>
+        <a href="/login">Login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
