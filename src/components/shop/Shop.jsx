827b90f11299454cc9edd7bc235c4eb4c0258ee0
Safoun <safoun.official@gmail.com>
2023-03-28 21:47:00 +0600

    done designing it

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -4,18 +4,22 @@ import("./Shop.css");
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    fetch("https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json")
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
+    )
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
     console.log(product);
+    console.log(cart);
     console.log(newCart);
   };
 
@@ -23,11 +27,30 @@ const Shop = () => {
     <div className="shop-container">
       <div className="products-container">
         {products.map((product) => (
-          <Product key={product.id} product={product} addToCart={addToCart}></Product>
+          <Product
+            key={product.id}
+            product={product}
+            addToCart={addToCart}
+          ></Product>
         ))}
       </div>
+
       <div className="cart-container">
-        <div>Order summary !!!</div> 
+        <div className="cart-header">Order Summary</div>
+
+        <div className="calculative-data">
+          <div>Selected items : {cart.length}</div>
+          <div>Total Price : ${cart.length}</div>
+          <div>Total Shipping Charge : ${cart.length}</div>
+          <div>Tax : ${cart.length}</div>
+        </div>
+
+        <div className="grand-total">Grand Total : ${cart.length}</div>
+
+        <div className="btn-s">
+          <button className="btn-clear">Clear Cart</button>
+          <button className="btn-review">Review Order</button>
+        </div>
       </div>
     </div>
   );
