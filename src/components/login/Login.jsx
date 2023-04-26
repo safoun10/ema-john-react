14d3665d9409a9a3906c60d8e6d536cd4e1429fd
Safoun <safoun.official@gmail.com>
2023-04-26 10:05:20 +0600

    added login style

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import "./Login.css";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <div>Login coming soon</div>
+        <div className='container'>
+            <div className='signUpBox'>
+
+                <div className='title'> Login </div>
+
+                <div className='input-box'>
+                    <div className='tag'>Email</div>
+                    <input className='input' type="email" name="email" required />
+                </div>
+
+                <div className='input-box'>
+                    <div className='tag'>Password</div>
+                    <input className='input' type="password" name="password" required />
+                </div>
+
+                <div>
+                    <button className="btn-sign-up">Login</button>
+                </div>
+
+                <div>
+                    New to Ema-John ? <Link className='login-txt' to="/sign-up">Sign Up</Link>
+                </div>
+
+                <div className='trick'>
+                    <div><hr className='hr' /></div>
+                    <div>or</div>
+                    <div><hr className='hr' /></div>
+                </div>
+
+                <div>
+                    <button className='g-btn'>
+                        <img src="../../../src/images/google.svg" className='g-img' />
+                        Continue with Google
+                    </button>
+                </div>
+
+            </div>
         </div>
     );
 };
